dafe8543ad5581dda145b77189e3e32efb35dd45
zhongleqi <98668159+zhongleqi@users.noreply.github.com>
2022-04-04 13:25:26 -0700

    Update main.js

--- Leqi_extra_assignment_1/assets/main.js
+++ Leqi_extra_assignment_1/assets/main.js
@@ -1,8 +1,7 @@
 
 
 document.getElementById('backgroundcolor').addEventListener('click', function() {
-    console.log(111111)
-    document.body.style.backgroundColor = "Pink";
+    document.body.style.backgroundColor = 'Pink';
 });
 
 document.getElementById('fontcolor').addEventListener('click', function() {
@@ -14,8 +13,8 @@ document.getElementById('fontfamily').addEventListener('click', function() {
     document.body.style.fontFamily = 'monospace';
 });
 
-document.getElementById('replacement-button').addEventListener('click', function() {
-    document.getElementById('replacement').innerHTML = 'I am about to give up';
+document.getElementById('replace').addEventListener('click', function() {
+    document.getElementById('replace').innerHTML = 'I am about to give up'
 });
 
 document.getElementById('alert').addEventListener('click', function(event) {
